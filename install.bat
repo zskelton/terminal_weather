f76a6fba428e3e9ec5eda125833ec298b7b58d3e
Zachary D. Skelton <zbert@hotmail.com>
2022-02-06 11:10:21 -0600

    feat(installer): Added install script and NPM cmd

---
+++ install.bat
@@ -0,0 +1,12 @@
+@ ECHO OFF
+REM Installer
+REM Skelton Netwokrs © 2022
+REM Copies Exectuable to User's Bin File
+
+:: Copy Program
+ECHO Moving to ~/Bin...
+COPY .\build\web.exe C:\Users\zbert\Bin\weather.exe
+
+:: Tagline
+ECHO.
+ECHO Created by Skelton Networks ©2022
